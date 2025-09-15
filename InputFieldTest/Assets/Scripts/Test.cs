41b3c019b300a9b65fe21d3a8c8e9708c0139c70
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-09-15 22:53:05 +0900

    esc 눌리면, 왜 onSubmit 이 발생하는가.

--- InputFieldTest/Assets/Scripts/Test.cs
+++ InputFieldTest/Assets/Scripts/Test.cs
@@ -13,12 +13,19 @@ public class Test : MonoBehaviour
         inputField.onSubmit.AddListener(Log);
     }
 
+    private void Update()
+    {
+        Debug.Log($"Update wasESC : {wasESC}  keyboadESC : {keyboadESC} wasCanceled : {wasCanceled}");
+    }
+
     void Log(string text)
     {
         wasESC = Input.GetKeyDown(KeyCode.Escape);
-        keyboadESC = TouchScreenKeyboard;
+
+        TouchScreenKeyboard kbd = inputField != null ? inputField.touchScreenKeyboard : null;
+        keyboadESC = kbd != null && kbd.status == TouchScreenKeyboard.Status.Canceled;
         wasCanceled = inputField.wasCanceled;
 
-        Debug.Log($"wasESC : {wasESC}  keyboadESC : {keyboadESC} wasCanceled : {wasCanceled}");
+        Debug.Log($"UpdateLog wasESC : {wasESC}  keyboadESC : {keyboadESC} wasCanceled : {wasCanceled}");
     }
 }
