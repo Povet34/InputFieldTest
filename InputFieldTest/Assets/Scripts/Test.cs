b1a778f7310659a93e25c74a97b96105274f1445
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-09-17 20:33:43 +0900

    가변 inputfield 구현

--- InputFieldTest/Assets/Scripts/Test.cs
+++ InputFieldTest/Assets/Scripts/Test.cs
@@ -11,21 +11,60 @@ public class Test : MonoBehaviour
     private void Awake()
     {
         inputField.onSubmit.AddListener(Log);
+        inputField.onTouchScreenKeyboardStatusChanged.AddListener(HandleKeyboardStatusChange);
+
     }
 
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            wasESC = true;
+        }
+
+        if(inputField.touchScreenKeyboard?.status == TouchScreenKeyboard.Status.Canceled)
+        {
+            keyboadESC = true;
+        }
+
+        if(inputField.wasCanceled)
+        {
+            wasCanceled = true;
+        }
+
         Debug.Log($"Update wasESC : {wasESC}  keyboadESC : {keyboadESC} wasCanceled : {wasCanceled}");
     }
 
     void Log(string text)
     {
-        wasESC = Input.GetKeyDown(KeyCode.Escape);
-
-        TouchScreenKeyboard kbd = inputField != null ? inputField.touchScreenKeyboard : null;
-        keyboadESC = kbd != null && kbd.status == TouchScreenKeyboard.Status.Canceled;
-        wasCanceled = inputField.wasCanceled;
 
         Debug.Log($"UpdateLog wasESC : {wasESC}  keyboadESC : {keyboadESC} wasCanceled : {wasCanceled}");
+
+        wasESC = false;
+        keyboadESC = false;
+        wasCanceled = false;
+
+    }
+
+    private void HandleKeyboardStatusChange(TouchScreenKeyboard.Status status)
+    {
+        if(TouchScreenKeyboard.visible)
+            Debug.Log("TouchScreenKeyboard is visible.");
+        else
+            Debug.Log("TouchScreenKeyboard is not visible.");
+
+        if (status == TouchScreenKeyboard.Status.Canceled)
+        {
+            keyboadESC = true;
+            Debug.Log("TouchScreenKeyboard was canceled by the user.");
+        }
+        else if (status == TouchScreenKeyboard.Status.Done)
+        {
+            Debug.Log("TouchScreenKeyboard input was submitted.");
+        }
+        else
+        {
+            Debug.Log("Keyboard status changed to: " + status);
+        }
     }
 }
